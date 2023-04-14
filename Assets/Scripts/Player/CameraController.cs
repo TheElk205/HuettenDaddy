3c548a06e52ffc0c729ce95241ca950a326a1a6b
Ferdinand Koeppen <ferdinand.koeppen@sensolligent.com>
2023-04-14 15:53:21 +0200

    add: first parallax and smooth camera movement
    Bacgorund images need to be exchanged for aprallax to work properly

--- Assets/Scripts/Player/CameraController.cs
+++ Assets/Scripts/Player/CameraController.cs
@@ -7,12 +7,13 @@ public class CameraController : MonoBehaviour
     public Camera camera;
     public Transform followMe;
     private float initialCameraZ = 0.0f;
-    
+    private Vector3 velocity = Vector3.zero;
+    public float smoothTime = 0.25f;
     public Vector3 cameraOffset;
     // Start is called before the first frame update
     void Start()
     {
-        cameraOffset = followMe.position - camera.transform.position;
+        //cameraOffset = followMe.position - camera.transform.position;
         initialCameraZ = camera.transform.position.z;
     }
 
@@ -22,6 +23,6 @@ public class CameraController : MonoBehaviour
         Vector3 newPosition = followMe.position + cameraOffset;
         newPosition.z = initialCameraZ;
 
-        camera.transform.position = newPosition;
+        camera.transform.position = Vector3.SmoothDamp(camera.transform.position, newPosition, ref velocity, smoothTime);
     }
 }
